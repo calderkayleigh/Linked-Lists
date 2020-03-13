51428a7ee6c707d72162f9530cc020fc96b2be01
Kayleigh Calder <59836279+calderkayleigh@users.noreply.github.com>
2020-03-13 18:52:20 -0400

    Update LNode.java

--- LNode.java
+++ LNode.java
@@ -1,12 +1,18 @@
-public class LNode {
+public class LNode 
+{
   public String name;
   public double latitude;
   public double longitude;
   public LNode next;
   /* You may not add more data members to this class. */
   
-  public LNode(String name, double latitude, double longitude) {
-    // fill in
+  public LNode(String name, double latitude, double longitude) 
+  {
+    this.name = name;
+    this.latitude = latitude; 
+    this.longitude = longitude;
+    this.next = null;
+    
   }
   
   
